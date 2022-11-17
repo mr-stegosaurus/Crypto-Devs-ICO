2da62c7dca1e210d482f71595e596284363114f3
Mr Stegosaurus <mrstego123@gmail.com>
2022-11-16 22:30:05 -0500

    fix code typos;

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -2,7 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
-const PRIVATE_KEY process.env.PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
